d99d5695768ad96ba18e9faa1974ade68cc0ca0b
CharlesCarley <snailrose@gmail.com>
2021-04-21 15:01:13 -0600

    Added a means to include test files.

--- Test/Test1.cpp
+++ Test/Test1.cpp
@@ -1,8 +1,11 @@
+#include <cstdio>
 #include "gtest/gtest.h"
-
+#include "TestDirectory.h"
 
 GTEST_TEST(Test1, AlwaysTrue)
 {
+    puts(ABSOLUTE_TEST_DIRECTORY);
+    puts(GetTestFilePath("inp.ans"));
     EXPECT_EQ(1, 1);
 }
 
